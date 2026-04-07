75bcab017dc55fc6daf8a7344bd5904b66265f49
murilokrebsky-factored <murilo.krebsky@factored.ai>
2026-04-07 17:10:17 -0300

    Fix duplicates on stg layer and add source freshness

--- models/staging/espn/stg_nba_games.sql
+++ models/staging/espn/stg_nba_games.sql
@@ -3,6 +3,17 @@ with source as (
     select * from {{ source('nba_raw', 'raw_nba_scoreboard') }}
 ),
 
+deduplicated as (
+    select 
+        *,
+        -- This creates a 'rank' for every row with the same game_id
+        row_number() over (
+            partition by game_id 
+            order by ingested_at desc -- or any timestamp/ID that shows which came last
+        ) as row_num
+    from source
+),
+
 renamed as (
     select
         game_id,
@@ -17,7 +28,8 @@ renamed as (
             when cast(home_score as int) > cast(away_score as int) then home_team
             when cast(home_score as int) < cast(away_score as int) then away_team
         end as winner_name
-    from source
+    from deduplicated
+    where row_num = 1
 )
 
 select * from renamed
