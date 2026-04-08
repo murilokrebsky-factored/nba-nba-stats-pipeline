2e7962fbac471cf315d70e1a91da02d82d35de4c
murilokrebsky-factored <murilo.krebsky@factored.ai>
2026-04-08 12:13:40 -0300

    Add YAML files for each model, adding descriptions and tests.

--- models/staging/espn/stg_nba_games.sql
+++ models/staging/espn/stg_nba_games.sql
@@ -1,6 +1,6 @@
 with source as (
     -- This points to raw.nba_raw.raw_nba_scoreboard via your sources.yml (name.table_name)
-    select * from {{ source('nba_raw', 'raw_nba_scoreboard') }}
+    select * except(venue) from {{ source('nba_raw', 'raw_nba_scoreboard') }}
 ),
 
 deduplicated as (
@@ -22,7 +22,6 @@ renamed as (
         cast(home_score as int) as home_score,
         away_team,
         cast(away_score as int) as away_score,
-        venue,
         -- Calculate the winner here so you don't have to do it later
         case 
             when cast(home_score as int) > cast(away_score as int) then home_team
